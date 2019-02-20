f1b6afd546e3cd05b0d8d17b7830f72f61b9b417
Tharles de Sousa Andrade <irtharles@gmail.com>
2019-02-20 10:55:38 -0300

    fix compile

--- deployment/1_ess_cloud_compile.sh
+++ deployment/1_ess_cloud_compile.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "\nCompiling ESS Cloud Server!\n"
+echo -e "\nCompiling ESS Cloud Server!\n"
 
 cd $HOME/EmergentSortService/pal/
 
 dnc . -v -sp "../repository;../Sort"
 
-echo "\nCompiling directory Sort!\n"
+echo -e "\nCompiling directory Sort!\n"
 cd .. ; cd Sort/
 dnc . -v
 
-echo "\nCompiling directory repository!\n"
+echo -e "\nCompiling directory repository!\n"
 cd .. ; cd repository/
 dnc . -v
 
-echo "\nRemoving file remoteSort.o!\n"
+echo -e "\nRemoving file remoteSort.o!\n"
 rm $HOME/EmergentSortService/Sort/sort/remoteSort.o
 
-echo "\nCompilation finish!\n"
+echo -e "\nCompilation finish!\n"
